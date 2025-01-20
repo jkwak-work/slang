f59bc83a6ac36610873c20fbca8d44abe9c86834
Jay Kwak <82421531+jkwak-work@users.noreply.github.com>
2025-01-20 08:46:58 -0800

    Support cooperative vector without Vulkan-header update
    Adding a Slang support for cooperative vector.
    But this commit doesn't have Vulkan-header update.

---
+++ source/slang/slang-ir-lower-coopvec.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace Slang {
+
+struct IRModule;
+class DiagnosticSink;
+
+/// Lower Cooperative Vectors to ordinary arrays
+void lowerCooperativeVectors(IRModule* module, DiagnosticSink* sink);
+
+} // namespace Slang
