f20359781437ede822529a081e0c267825540da4
Jay Kwak <82421531+jkwak-work@users.noreply.github.com>
2025-02-26 13:33:55 -0800

    Adding comments

--- source/slang/slang-emit-spirv.cpp
+++ source/slang/slang-emit-spirv.cpp
@@ -1914,6 +1914,8 @@ struct SPIRVEmitContext : public SourceEmitterBase, public SPIRVEmitSharedContex
                 auto newType = emitOpTypeNodePayloadArray(
                     inst, nodePayloadArrayType->getRecordType());
 
+                // TODO: This is a temporary hack.
+                // The NodeID must come from an attribute [NodeID("name")].
                 Slang::StringBuilder str;
                 str << "NodeID_" << uint32_t(nodePayloadArrayType->getNodeID()->getValue());
                 SpvInst* spvStr = emitOpConstantString(nullptr, str.getUnownedSlice());
@@ -7767,7 +7769,6 @@ struct SPIRVEmitContext : public SourceEmitterBase, public SPIRVEmitSharedContex
                         return getSection(SpvLogicalSectionID::Annotations);
                     case SpvOpTypeNodePayloadArrayAMDX:
                         return getSection(SpvLogicalSectionID::ConstantsAndTypes);
-
                     default:
                         return defaultParent;
                     }
