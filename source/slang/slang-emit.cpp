d9689d5dd2bc32a5fffd110d95b098c01ead60d5
Jay Kwak <82421531+jkwak-work@users.noreply.github.com>
2025-06-07 00:20:26 -0700

    Replace the C style enable/disable with RAII

--- source/slang/slang-emit.cpp
+++ source/slang/slang-emit.cpp
@@ -921,9 +921,10 @@ Result linkAndOptimizeIR(
         if (requiredLoweringPassSet.autodiff)
         {
             dumpIRIfEnabled(codeGenContext, irModule, "BEFORE-AUTODIFF");
-            enableIRValidationAtInsert();
-            changed |= processAutodiffCalls(targetProgram, irModule, sink);
-            disableIRValidationAtInsert();
+            {
+                auto validationScope = enableIRValidationScope();
+                changed |= processAutodiffCalls(targetProgram, irModule, sink);
+            }
             dumpIRIfEnabled(codeGenContext, irModule, "AFTER-AUTODIFF");
         }
 
