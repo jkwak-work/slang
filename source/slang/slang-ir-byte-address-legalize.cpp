5e5a56d34c1f5c5b9a44a6ce556daf366df84d51
Jay Kwak <82421531+jkwak-work@users.noreply.github.com>
2025-10-24 17:20:18 -0700

    Handle IRFunc and IRCall param cases

--- source/slang/slang-ir-byte-address-legalize.cpp
+++ source/slang/slang-ir-byte-address-legalize.cpp
@@ -1602,37 +1602,63 @@ struct ByteAddressBufferLegalizationContext
             if (auto babParam = as<IRParam>(babIR))
             {
                 auto parentBlock = as<IRBlock>(babIR->getParent());
+                auto paramIndex = parentBlock->getParamIndex(babParam);
+                if (paramIndex < 0)
+                    continue;
+
+                UInt argOperandIndex = 1 + paramIndex;
 
                 // Handle Phi-node IRParam
-                auto paramIndex = parentBlock->getParamIndex(babParam);
-                if (paramIndex >= 0)
+                for (auto use = parentBlock->firstUse; use; use = use->nextUse)
                 {
-                    UInt argOperandIndex = 1 + paramIndex;
+                    auto user = use->getUser();
+                    auto branchIR = as<IRUnconditionalBranch>(user);
+                    if (!branchIR)
+                        continue;
 
-                    for (auto use = parentBlock->firstUse; use; use = use->nextUse)
+                    auto targetBlock = branchIR->getTargetBlock();
+                    if (targetBlock != parentBlock)
+                        continue;
+
+                    auto argBranch = branchIR->getOperand(argOperandIndex);
+                    if (byteAddrBufferToReplace.containsKey(argBranch))
+                        continue;
+
+                    auto argType = argBranch->getDataType();
+                    if (isTypeLegalForByteAddressLoadStore(argType))
+                    {
+                        // Element type should be same.
+                        SLANG_ASSERT(argType == validBufferType);
+                        continue;
+                    }
+
+                    byteAddrBufferToReplace[argBranch] = validBufferType;
+                    workList.add(argBranch);
+                }
+
+                if (auto func = as<IRFunc>(parentBlock->getParent()))
+                {
+                    for (auto use = func->firstUse; use; use = use->nextUse)
                     {
                         auto user = use->getUser();
-                        auto branchIR = as<IRUnconditionalBranch>(user);
-                        if (!branchIR)
+                        auto call = as<IRCall>(user);
+                        if (!call)
                             continue;
 
-                        auto targetBlock = branchIR->getTargetBlock();
-                        if (targetBlock != parentBlock)
+                        auto argCall = call->getOperand(argOperandIndex);
+                        if (byteAddrBufferToReplace.containsKey(argCall))
                             continue;
 
-                        auto newItem = branchIR->getOperand(argOperandIndex);
-                        if (byteAddrBufferToReplace.containsKey(newItem))
-                            continue;
-
-                        if (!as<IRByteAddressBufferTypeBase>(newItem->getDataType()))
+                        auto argType = argCall->getDataType();
+                        if (isTypeLegalForByteAddressLoadStore(argType))
                         {
                             // Element type should be same.
-                            SLANG_ASSERT(babIRType == validBufferType);
+                            SLANG_ASSERT(argType == validBufferType);
                             continue;
                         }
 
-                        byteAddrBufferToReplace[newItem] = validBufferType;
-                        workList.add(newItem);
+                        byteAddrBufferToReplace[argCall] = validBufferType;
+                        workList.add(argCall);
                     }
                 }
             }
